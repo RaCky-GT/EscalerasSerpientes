e92740853ecbbca74b29a5398251b602d4923cda
RaCky-GT <rguarchajt2@gmail.com>
2025-09-24 00:56:40 -0600

    Se agrega la funcionalidad de visualizacion de menu e impresion de mapa o tablero

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int option = 0;
+        do{
+            System.out.println("======== MENU ========");
+            System.out.println(" 1. In1ciar Juego");
+            System.out.println(" 2. Retomar Juego");
+            System.out.println(" 3. Generar Reportes");
+            System.out.println(" 4. Salir");
+            option = sc.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            switch (option){
+                case 1:
+                    tableroControlador.dibujar();
+                    break;
+
+            }
+
+        }while (option!=4);
+        System.out.println("Se finaliza el juego.");
+        sc.close();
     }
+
+
 }
